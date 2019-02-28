49508b6fd29c409b60d80d03adfa987faac66250
Mark Harris <mark.harris@hivehome.com>
2019-02-28 15:31:02 +0000

    BIQLITE-201 Version the javascript package.

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -5,6 +5,7 @@ sbt clean +publishLocal
 popd
 sbt clean +publish
 pushd npm-package
+sed -i 's/"version": ".*"/"version": "'"${BUILD_NUMBER}"'"/g' package.json
 npm pack
 popd
 tar -czvf retrofit-parameter-names-${BUILD_NUMBER}.tar.gz /src/main/resources/parameters.csv
